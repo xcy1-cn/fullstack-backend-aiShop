0e6cb0cd3519e06fff83682fe19d6987b6672bcc
xcy <1611517164@qq.com>
2026-04-06 20:16:07 +0800

    2026.4.6 night

--- mock/comments.js
+++ mock/comments.js
@@ -21,6 +21,36 @@ const comments = {
         content: "降噪效果很强，佩戴也舒服。",
         createTime: "2026-04-03",
     }, ],
+    101: [{
+            id: 101,
+            username: "用户A",
+            score: 5,
+            content: "拍照很好，运行流畅，质感很强。",
+            createTime: "2026-04-01",
+        },
+        {
+            id: 102,
+            username: "用户B",
+            score: 4,
+            content: "续航还不错，价格略高。",
+            createTime: "2026-04-02",
+        },
+    ],
+    102: [{
+            id: 101,
+            username: "用户A",
+            score: 5,
+            content: "拍照很好，运行流畅，质感很强。",
+            createTime: "2026-04-01",
+        },
+        {
+            id: 102,
+            username: "用户B",
+            score: 4,
+            content: "续航还不错，价格略高。",
+            createTime: "2026-04-02",
+        },
+    ],
 };
 
 module.exports = comments;
